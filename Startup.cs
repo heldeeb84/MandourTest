3b9e482dee25a749c45a3f353d218378e96b723e
heldeeb84 <47702184+heldeeb84@users.noreply.github.com>
2020-10-28 15:12:15 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mandour.Startup))]
+namespace Mandour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
